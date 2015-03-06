f1313daf6047cb76b90e76f876e09dd8032dcde6
Volker Berlin <volker.berlin@googlemail.com>
2015-03-06 17:33:22 +0100

    fix a bug with spaces in extended selectors

--- src/com/inet/lib/less/SelectorTokenizer.java
+++ src/com/inet/lib/less/SelectorTokenizer.java
@@ -1,7 +1,7 @@
 /**
  * MIT License (MIT)
  *
- * Copyright (c) 2014 Volker Berlin
+ * Copyright (c) 2014 - 2015 Volker Berlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -54,7 +54,7 @@ class SelectorTokenizer {
                     break LOOP;
             }
         } while( true );
-        String str = selector.substring( lastIdx, idx );
+        String str = selector.substring( lastIdx, idx ).trim();
         lastIdx = idx;
         return str;
     }
