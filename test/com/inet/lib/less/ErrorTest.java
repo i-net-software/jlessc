3b75cb49eac536d07cc4302194dd64a34d880991
Volker Berlin <volker.berlin@googlemail.com>
2016-02-27 17:55:02 +0100

    More error test case to improve the coverage.

--- test/com/inet/lib/less/ErrorTest.java
+++ test/com/inet/lib/less/ErrorTest.java
@@ -6,38 +6,89 @@ import static org.junit.Assert.*;
 
 public class ErrorTest {
 
-    @Test
-    public void parenthesisWithComma() {
-        Less.compile( null, ".a { a: (red); }", false );
+    private void assertLessException( String less, String expectedErrorMessage ) {
         try {
-            Less.compile( null, ".a { a: (red,green); }", false );
+            Less.compile( null, less, false );
             fail( "LessException expected" );
         } catch( LessException lex ) {
             String message = lex.getMessage();
-            assertEquals( "Unrecognised input", message.substring( 0, message.indexOf( '\n' ) ) );
+            assertEquals( expectedErrorMessage, message.substring( 0, message.indexOf( '\n' ) ) );
         }
+    }
+    @Test
+    public void parenthesisWithComma() {
+        Less.compile( null, ".a { a: (red); }", false );
+        assertLessException( ".a { a: (red,green); }", "Unrecognized input" );
     }
 
     @Test
     public void maxDiffTypes() {
-        try {
-            Less.compile( null, ".a { a: max( 1px, 1% ); }", false );
-            fail( "LessException expected" );
-        } catch( LessException lex ) {
-            String message = lex.getMessage();
-            assertEquals( "Incompatible types", message.substring( 0, message.indexOf( '\n' ) ) );
-        }
+        assertLessException( ".a { a: max( 1px, 1% ); }", "Incompatible types" );
     }
 
     @Test
     public void minDiffTypes() {
-        try {
-            Less.compile( null, ".a { a: min( 1px, 1% ); }", false );
-            fail( "LessException expected" );
-        } catch( LessException lex ) {
-            String message = lex.getMessage();
-            assertEquals( "Incompatible types", message.substring( 0, message.indexOf( '\n' ) ) );
-        }
+        assertLessException( ".a { a: min( 1px, 1% ); }", "Incompatible types" );
+    }
+
+    @Test
+    public void unrecognizedInput1() {
+        assertLessException( "a: > 5;", "Unrecognized input: '>'" );
+    }
+
+    @Test
+    public void unrecognizedInput2() {
+        assertLessException( "a: 1 <> 5;", "Unrecognized input: '>'" );
+    }
+
+    @Test
+    public void unrecognizedInput3() {
+        assertLessException( "a:}", "Unrecognized input: 'a:}'" );
+    }
+
+    @Test
+    public void unrecognizedInput4() {
+        assertLessException( "a:);", "Unrecognized input: 'a:)'" );
+    }
+
+    @Test
+    public void unrecognizedInput5() {
+        assertLessException( "/*comment", "Unrecognized input: '/*comment'" );
+    }
+
+    @Test
+    public void unrecognizedInput6() {
+        assertLessException( "@{a;", "Unrecognized input: '@{a;'" );
+    }
+
+    @Test
+    public void unrecognizedInput7() {
+        assertLessException( ".a()xyz{}", "Unrecognized input: 'xyz{'" );
+    }
+
+    @Test
+    public void unknownImportKeyword() {
+        assertLessException( "@import (xyz) 'c.data';", "Unknown @import keyword: xyz" );
+    }
+
+    @Test
+    public void undefineVariableInSelectorInput() {
+        assertLessException( "a@{b}c{a:1;}", "Undefine Variable: @b in a@{b}c" );
+    }
+
+    @Test
+    public void unexpectedEOF1() {
+        assertLessException( "@a:'a", "Unexpected end of Less data" );
+    }
+
+    @Test
+    public void unexpectedEOF2() {
+        assertLessException( "a{b", "Unexpected end of Less data" );
+    }
+
+    @Test
+    public void propsInRoot() {
+        assertLessException( "a: 5;", "Properties must be inside selector blocks, they cannot be in the root." );
     }
 
     /**
