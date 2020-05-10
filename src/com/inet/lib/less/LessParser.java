c9a997f4fd0820eb85c429c10ffe8b9587d920f0
Volker Berlin <volker.berlin@googlemail.com>
2020-05-10 12:40:34 +0200

    A selector with `:extend` is never a mixin. fix #67

--- src/com/inet/lib/less/LessParser.java
+++ src/com/inet/lib/less/LessParser.java
@@ -1,7 +1,7 @@
 /**
  * MIT License (MIT)
  *
- * Copyright (c) 2014 - 2019 Volker Berlin
+ * Copyright (c) 2014 - 2020 Volker Berlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -234,7 +234,7 @@ class LessParser implements FormattableContainer {
                     }
                     break;
                 case '(':
-                    if( !reader.nextIsMixinParam( false ) ) {
+                    if( !reader.nextIsMixinParam( false ) || builder.indexOf( ":extend" ) >= 0 ) {
                         builder.append( ch );
                         break;
                     }
